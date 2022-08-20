797781cb352c580d13ad125c101d0ffa21abc312
jack <jack@jpg.space>
2022-08-20 12:44:40 -0500

    contract compiles successfully, add back ownable

---
+++ packages/backend/contracts/NFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+// import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract SellOutNFT is ERC1155 {
+    uint256 public totalSupply;
+
+    constructor() ERC1155("https://sellout-ten.vercel.app/nft.json") {}
+
+    function mint() public {
+        unchecked {
+            ++totalSupply;
+        }
+        _mint(msg.sender, 1, 1, "");
+    }
+}
--- packages/backend/deploy/00_deploy_contract.ts
+++ packages/backend/deploy/00_deploy_contract.ts
@@ -1,19 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-const main: DeployFunction = async function ({getNamedAccounts, deployments}: HardhatRuntimeEnvironment) {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+const main: DeployFunction = async function ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) {
+	const { deploy } = deployments;
+	const { deployer } = await getNamedAccounts();
 
-  const args = ['Hello!!!!!!!!'];
-  await deploy('Greeter', {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-    args: args,
-    from: deployer,
-    log: true,
-  });
+	// const args = ['Hello!!!!!!!!'];
+	// await deploy('Greeter', {
+	//   // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+	//   args: args,
+	//   from: deployer,
+	//   log: true,
+	// });
+	await deploy('SellOutNFT', { from: deployer, log: true });
 };
 
 export default main;
- 
+
 export const tags = ['all', 'greeter'];
